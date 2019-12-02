8178a893002783a3dbb6aeae896be3758e767a4c
Mhlengi Mzimela <mhlengi@digsconnect.com>
2019-12-02 11:21:49 +0200

    Updated settings file

--- tangent_solutions/settings.py
+++ tangent_solutions/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -154,14 +155,8 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-#  Add configuration for static files storage using white-noise
+# Add configuration for static files storage using white-noise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-# import dj_database_url
-# prod_db = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
-
-import django_heroku
-
+# Django heroku configuration
 django_heroku.settings(locals())
-
